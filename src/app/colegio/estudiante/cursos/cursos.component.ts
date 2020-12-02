4deba93fef978798db5868d79099dd8293c27c53
Daniel Rodriguez <jdrodriguez@grupotecun.com>
2020-12-02 12:20:06 -0600

    :art: se agrega formato a modulo estudiante

--- src/app/colegio/estudiante/cursos/cursos.component.ts
+++ src/app/colegio/estudiante/cursos/cursos.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {ActivatedRoute, Params, Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Location } from '@angular/common';
 
-import { CursosService } from "../_services/cursos.service";
+import { CursosService } from '../_services/cursos.service';
 import { NotificationsService } from 'angular2-notifications';
 import { Subject } from 'rxjs/Rx';
 import 'rxjs/add/operator/switchMap';
@@ -13,71 +13,68 @@ import 'rxjs/add/operator/switchMap';
   styleUrls: ['./cursos.component.css']
 })
 export class CursosComponent implements OnInit {
-  @Input() id:any;
-  Table:any
-  selectedData:any
-  Id:any = localStorage.getItem('currentIdStudent');
-  tipo:any
+  @Input() id: any;
+  Table: any;
+  selectedData: any;
+  Id: any = localStorage.getItem('currentIdStudent');
+  tipo: any;
+  public options = {
+    position: ['bottom', 'right'],
+    timeOut: 2000,
+    lastOnBottom: false,
+    animate: 'fromLeft',
+    showProgressBar: false,
+    pauseOnHover: true,
+    clickToClose: true,
+    maxLength: 200
+  };
   constructor(
     private _service: NotificationsService,
     private route: ActivatedRoute,
-    private location:Location,
-    private router:Router,
+    private location: Location,
+    private router: Router,
     private mainService: CursosService
   ) { }
-  
-    ngOnInit() {
-      this.route.params
+
+  ngOnInit() {
+    this.route.params
       .switchMap((params: Params) => (params['op']))
-      .subscribe(response => { 
-                        this.tipo=response
-                    });
-      this.cargarAll()
-    }
-    cargarAll(){
-      $('#Loading').css('display','block')
-    $('#Loading').addClass('in')
+      .subscribe(response => {
+        this.tipo = response;
+      });
+    this.cargarAll();
+  }
+  cargarAll() {
+    $('#Loading').css('display', 'block');
+    $('#Loading').addClass('in');
     this.mainService.getAll(this.Id)
-                        .then(response => {
-                          this.Table = response
-                          console.clear 
-                          $('#Loading').css('display','none')
-                        }).catch(error => {
-                          console.clear     
-                          this.createError(error) 
-                        })
-    }
-    cargarSingle(id:number){
-      $('#Loading').css('display','block')
-    $('#Loading').addClass('in')
+      .then(response => {
+        this.Table = response;
+        $('#Loading').css('display', 'none');
+      }).catch(error => {
+        this.createError(error);
+      });
+  }
+  cargarSingle(id: number) {
+    $('#Loading').css('display', 'block');
+    $('#Loading').addClass('in');
     this.mainService.getSingle(id)
-                        .then(response => {
-                          this.selectedData = response;
-                          $('#Loading').css('display','none')
-                        }).catch(error => {
-                          console.clear     
-                          this.createError(error) 
-                        })
-    }
+      .then(response => {
+        this.selectedData = response;
+        $('#Loading').css('display', 'none');
+      }).catch(error => {
+        this.createError(error);
+      });
+  }
 
-    
-  public options = {
-               position: ["bottom", "right"],
-               timeOut: 2000,
-               lastOnBottom: false,
-               animate: "fromLeft",  
-               showProgressBar: false,
-               pauseOnHover: true,
-               clickToClose: true,
-               maxLength: 200
-           };
-  
-    create(success) {
-                this._service.success('¡Éxito!',success)
 
-    }
-    createError(error) {
-                this._service.error('¡Error!',error)
 
-    }
+  create(success) {
+    this._service.success('¡Éxito!', success);
+
+  }
+  createError(error) {
+    this._service.error('¡Error!', error);
+
+  }
 }
--- src/app/colegio/estudiante/estudiante.module.ts
+++ src/app/colegio/estudiante/estudiante.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from "@angular/forms";
-import { DataTableModule } from "angular2-datatable";
+import { FormsModule } from '@angular/forms';
+import { DataTableModule } from 'angular2-datatable';
 
 import { EstudianteRoutingModule } from './estudiante.routing';
 import { EstudianteComponent } from './estudiante.component';
