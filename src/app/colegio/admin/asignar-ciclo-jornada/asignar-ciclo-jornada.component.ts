ce9ab0c47081626d99a06cf78c61c8aa20ef96f4
Daniel Rodriguez <jdrodriguez@grupotecun.com>
2020-12-02 13:42:19 -0600

    :art: se aplica formato a componentes de asignacion de administrador

--- src/app/colegio/admin/asignar-ciclo-jornada/asignar-ciclo-jornada.component.ts
+++ src/app/colegio/admin/asignar-ciclo-jornada/asignar-ciclo-jornada.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from "@angular/router";
+import { Router, ActivatedRoute } from '@angular/router';
 
-import { CiclosJornadaService } from "../_services/_asignaciones/ciclos-jornada.service";
-import { CyclesService } from "../_services/cycles.service";
-import { StudyingDaysService } from "../_services/studying-days.service";
+import { CiclosJornadaService } from '../_services/_asignaciones/ciclos-jornada.service';
+import { CyclesService } from '../_services/cycles.service';
+import { StudyingDaysService } from '../_services/studying-days.service';
 import { NotificationsService } from 'angular2-notifications';
 
 @Component({
@@ -12,14 +12,24 @@ import { NotificationsService } from 'angular2-notifications';
   styleUrls: ['./asignar-ciclo-jornada.component.css']
 })
 export class AsignarCicloJornadaComponent implements OnInit {
-  Table:any
-  selectedData:any[]
-  childs:any[]
-  parentCombo:any
-  beginDate:any
-  endDate:any
+  Table: any;
+  selectedData: any;
+  childs: any[];
+  parentCombo: any;
+  beginDate: any;
+  endDate: any;
   public rowsOnPage = 5;
-  public search:any
+  public search: any;
+  public options = {
+    position: ['bottom', 'right'],
+    timeOut: 2000,
+    lastOnBottom: false,
+    animate: 'fromLeft',
+    showProgressBar: false,
+    pauseOnHover: true,
+    clickToClose: true,
+    maxLength: 200
+  };
   constructor(
     private _service: NotificationsService,
     private route: ActivatedRoute,
@@ -28,144 +38,111 @@ export class AsignarCicloJornadaComponent implements OnInit {
     private ChildsService: CyclesService,
     private ParentsService: StudyingDaysService
   ) { }
-    ngOnInit() {
-      let date = new Date();
-      let month = date.getMonth()+1;
-      let month2;
-      if(month<10){
-        month2='0'+month;
-      }else{
-        month2=month
-      }
-      this.beginDate= date.getFullYear()+'-'+month2+'-01'
-      month = date.getMonth()+2;
-      if(month<10){
-        month2='0'+month;
-      }else{
-        month2=month;
-      }
-      this.endDate=date.getFullYear()+'-'+month2+'-01'
-      
-      
-      this.cargarAll()
-      this.ChildsService.getAll()
-                        .then(response => {
-                          this.childs = response
-                          
-                          console.clear 
-                        }).catch(error => {
-                          console.clear     
-                          this.createError(error) 
-                        })
-      this.ParentsService.getAll()
-                        .then(response => {
-                          this.parentCombo = response
-                          
-                          console.clear 
-                        }).catch(error => {
-                          console.clear     
-                          this.createError(error) 
-                        })
+  ngOnInit() {
+    const date = new Date();
+    let month = date.getMonth() + 1;
+    let month2;
+    if (month < 10) {
+      month2 = '0' + month;
+    } else {
+      month2 = month;
     }
-    
-    
-    cargarAll(){
-      $('#Loading').css('display','block')
-      $('#Loading').addClass('in')
-      this.mainService.getAll()
-                        .then(response => {
-                          this.Table = response
-                          $("#editModal .close").click();
-                          $("#insertModal .close").click();
-                          $('#Loading').css('display','none')
-                          console.clear 
-                        }).catch(error => {
-                          console.clear     
-                          this.createError(error) 
-                        })
+    this.beginDate = date.getFullYear() + '-' + month2 + '-01';
+    month = date.getMonth() + 2;
+    if (month < 10) {
+      month2 = '0' + month;
+    } else {
+      month2 = month;
     }
-    cargarSingle(id:number){
-      $('#Loading').css('display','block')
-      $('#Loading').addClass('in')
-      this.mainService.getSingle(id)
-                        .then(response => {
-                          this.selectedData = response
-                          console.clear 
-                                                    
-                          $('#Loading').css('display','none')
-                        }).catch(error => {
-                          console.clear     
-                          this.createError(error) 
-                        })
-    }
-    update(formValue:any){
-      $('#Loading').css('display','block')
-      $('#Loading').addClass('in')
-      //console.log(data)
-      this.mainService.update(formValue)
-                        .then(response => {
-                          this.cargarAll()
-                          console.clear 
-                          this.create('Master Actualizado')
-                          $('#Loading').css('display','none')
-                        }).catch(error => {
-                          console.clear     
-                          this.createError(error) 
-                        })
-      
-    }
-    delete(id:string){
-      $('#Loading').css('display','block')
-      $('#Loading').addClass('in')
-      this.mainService.delete(id)
-                        .then(response => {
-                          this.cargarAll()
-                          console.clear 
-                          this.create('Master Borrado')
-                          $('#Loading').css('display','none')
-                          
-                        }).catch(error => {
-                          console.clear     
-                          this.createError(error) 
-                        })
-      
-    }
-    
-    insert(formValue:any){
-      $('#Loading').css('display','block')
-      $('#Loading').addClass('in')
-      this.mainService.create(formValue)
-                        .then(response => {
-                          this.cargarAll()
-                          console.clear 
-                          this.create('Master Ingresado')
-                          $('#Loading').css('display','none')
-                          
-                        }).catch(error => {
-                          console.clear     
-                          this.createError(error) 
-                        })
-      
-      
-    }
-    
-  public options = {
-               position: ["bottom", "right"],
-               timeOut: 2000,
-               lastOnBottom: false,
-               animate: "fromLeft",  
-               showProgressBar: false,
-               pauseOnHover: true,
-               clickToClose: true,
-               maxLength: 200
-           };
-  
-    create(success) {
-                this._service.success('¡Éxito!',success)
+    this.endDate = date.getFullYear() + '-' + month2 + '-01';
 
-    }
-    createError(error) {
-                this._service.error('¡Error!',error)
 
-    }
+    this.cargarAll();
+    this.ChildsService.getAll()
+      .then(response => {
+        this.childs = response;
+      }).catch(error => {
+        this.createError(error);
+      });
+    this.ParentsService.getAll()
+      .then(response => {
+        this.parentCombo = response;
+
+      }).catch(error => {
+        this.createError(error);
+      });
+  }
+
+
+  cargarAll() {
+    $('#Loading').css('display', 'block');
+    $('#Loading').addClass('in');
+    this.mainService.getAll()
+      .then(response => {
+        this.Table = response;
+        $('#editModal .close').click();
+        $('#insertModal .close').click();
+        $('#Loading').css('display', 'none');
+      }).catch(error => {
+        this.createError(error);
+      });
+  }
+  cargarSingle(id: number) {
+    $('#Loading').css('display', 'block');
+    $('#Loading').addClass('in');
+    this.mainService.getSingle(id)
+      .then(response => {
+        this.selectedData = response;
+
+        $('#Loading').css('display', 'none');
+      }).catch(error => {
+        this.createError(error);
+      });
+  }
+  update(formValue: any) {
+    $('#Loading').css('display', 'block');
+    $('#Loading').addClass('in');
+    this.mainService.update(formValue)
+      .then(response => {
+        this.cargarAll();
+        this.create('Master Actualizado');
+        $('#Loading').css('display', 'none');
+      }).catch(error => {
+        this.createError(error);
+      });
+
+  }
+  delete(id: string) {
+    $('#Loading').css('display', 'block');
+    $('#Loading').addClass('in');
+    this.mainService.delete(id)
+      .then(response => {
+        this.cargarAll();
+        this.create('Master Borrado');
+        $('#Loading').css('display', 'none');
+
+      }).catch(error => {
+        this.createError(error);
+      });
+
+  }
+
+  insert(formValue: any) {
+    $('#Loading').css('display', 'block');
+    $('#Loading').addClass('in');
+    this.mainService.create(formValue)
+      .then(response => {
+        this.cargarAll();
+        this.create('Master Ingresado');
+        $('#Loading').css('display', 'none');
+      }).catch(error => {
+        this.createError(error);
+      });
+  }
+  create(success) {
+    this._service.success('¡Éxito!', success);
+  }
+  createError(error) {
+    this._service.error('¡Error!', error);
+  }
 }
--- src/app/colegio/admin/asignar-estudiantes-tutores/asignar-estudiantes-tutores.component.ts
+++ src/app/colegio/admin/asignar-estudiantes-tutores/asignar-estudiantes-tutores.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from "@angular/router";
+import { Router, ActivatedRoute } from '@angular/router';
 
-import { EstudiantesTutoresService } from "../_services/_asignaciones/estudiantes-tutores.service";
-import { StudentsService } from "../_services/students.service";
-import { TutorsService } from "../_services/tutors.service";
+import { EstudiantesTutoresService } from '../_services/_asignaciones/estudiantes-tutores.service';
+import { StudentsService } from '../_services/students.service';
+import { TutorsService } from '../_services/tutors.service';
 import { NotificationsService } from 'angular2-notifications';
 @Component({
   selector: 'app-asignar-estudiantes-tutores',
@@ -11,16 +11,27 @@ import { NotificationsService } from 'angular2-notifications';
   styleUrls: ['./asignar-estudiantes-tutores.component.css']
 })
 export class AsignarEstudiantesTutoresComponent implements OnInit {
-  Table:any
-  selectedData:any[]
-  droppedItemsId:any=[]
-  childs:any[]
-  childsId:any=[]
-  droppedItems:any=[]
-  parentCombo:any
-  selectedParent:any
+  Table: any;
+  selectedData: any[];
+  droppedItemsId: any = [];
+  childs: any[];
+  childsId: any = [];
+  droppedItems: any = [];
+  parentCombo: any;
+  selectedParent: any;
   public rowsOnPage = 5;
-  public search:any
+  public search: any;
+
+  public options = {
+    position: ['bottom', 'right'],
+    timeOut: 2000,
+    lastOnBottom: false,
+    animate: 'fromLeft',
+    showProgressBar: false,
+    pauseOnHover: true,
+    clickToClose: true,
+    maxLength: 200
+  };
   constructor(
     private _service: NotificationsService,
     private route: ActivatedRoute,
@@ -29,197 +40,169 @@ export class AsignarEstudiantesTutoresComponent implements OnInit {
     private ChildsService: StudentsService,
     private ParentsService: TutorsService
   ) { }
-    ngOnInit() {
-      
-      this.cargarAll()
-      this.cargarFree()
-      this.ParentsService.getAll()
-                        .then(response => {
-                          this.parentCombo = response
-                          
-                          console.clear 
-                        }).catch(error => {
-                          console.clear     
-                          this.createError(error) 
-                        })
-    }
-    onItemDrop(e: any) {
-        // Get the dropped data here 
-        if(!this.selectedParent){
-          this.createError("Debe seleccionar un tutor")
-        }else{
-          this.droppedItemsId.push({"id":e.dragData.id});
-          this.selectedData.push(e.dragData);
-          this.childs.splice(this.childs.findIndex(dat=>{
-            return dat.id==e.dragData.id
-          }),1)
-          this.childsId.splice(this.childsId.findIndex(dat=>{
-            return dat.id==e.dragData.id
-          }),1)
-        }
-    }
+  ngOnInit() {
 
-    onItemRemove(e: any) {
-        // Get the dropped data here 
-          
-          this.childsId.push({"id":e.dragData.id});
-          this.childs.push(e.dragData);
-          this.selectedData.splice(this.selectedData.findIndex(dat=>{
-            return dat.id==e.dragData.id
-          }),1)
-          this.droppedItemsId.splice(this.droppedItemsId.findIndex(dat=>{
-            return dat.id==e.dragData.id
-          }),1)
-        
+    this.cargarAll();
+    this.cargarFree();
+    this.ParentsService.getAll()
+      .then(response => {
+        this.parentCombo = response;
+      }).catch(error => {
+        this.createError(error);
+      });
+  }
+  onItemDrop(e: any) {
+    // Get the dropped data here
+    if (!this.selectedParent) {
+      this.createError('Debe seleccionar un tutor');
+    } else {
+      this.droppedItemsId.push({ 'id': e.dragData.id });
+      this.selectedData.push(e.dragData);
+      this.childs.splice(this.childs.findIndex(dat => {
+        return dat.id === e.dragData.id;
+      }), 1);
+      this.childsId.splice(this.childsId.findIndex(dat => {
+        return dat.id === e.dragData.id;
+      }), 1);
+    }
+  }
 
-        
-    }
-    limpiar(){
-      if(this.selectedData){
-        this.selectedData.length=0
-        this.droppedItemsId.length=0
-        this.droppedItems.length=0
-        this.selectedParent=null
-      }
-    }
-    cargarFree(){
-      $('#Loading').css('display','block')
-      $('#Loading').addClass('in')
-      this.ChildsService.getFreeStudents()
-                        .then(response => {
-                          this.childs = response
-                          this.childs.forEach((item,index)=>{
-                            this.childsId.push({"id":item.id});
-                          })
-                          console.clear 
-                        }).catch(error => {
-                          console.clear     
-                          this.createError(error) 
-                        })
-    }
-    cargarAll(){
-      $('#Loading').css('display','block')
-      $('#Loading').addClass('in')
-      this.ParentsService.getBussy()
-                        .then(response => {
-                          this.Table = response
-                          $("#editModal .close").click();
-                          $("#insertModal .close").click();
-                          $('#Loading').css('display','none')
-                          console.clear 
-                        }).catch(error => {
-                          console.clear     
-                          this.createError(error) 
-                        })
-    }
-    cargarSingle(id:number){
-      $('#Loading').css('display','block')
-      $('#Loading').addClass('in')
-      this.selectedParent=id
-      if(this.droppedItemsId.length){
-        this.droppedItemsId.length = 0;
-      }
-      this.cargarFree()
-      this.mainService.getMyChilds(id)
-                        .then(response => {
-                          this.selectedData = response
-                          this.selectedData.forEach((item,index)=>{
-                            this.droppedItemsId.push({"id":item.id});
-                          })
-                          $('#Loading').css('display','none')
-                          console.clear 
-                                                    
-                        }).catch(error => {
-                          console.clear     
-                          this.createError(error) 
-                        })
-    }
-    update(formValue:any){
-      $('#Loading').css('display','block')
-      $('#Loading').addClass('in')
-      //console.log(data)
-      this.mainService.update(formValue)
-                        .then(response => {
-                          this.cargarAll()
-                          console.clear 
-                          this.create('Ciclo Actualizado exitosamente')
-                          $('#Loading').css('display','none')
-                        }).catch(error => {
-                          console.clear     
-                          this.createError(error) 
-                        })
-      
-    }
-    delete2(id:string){
-      this.mainService.delete(id)
-                        .then(response => {
-                          this.cargarAll()
-                          console.clear 
-                          this.create('Estudiantes Desasignados')
-                        }).catch(error => {
-                          console.clear     
-                          this.createError(error) 
-                        })
-      
-    }
-    delete(formValue){
-      this.mainService.deleteAll(formValue)
-                        .then(response => {
-                          this.cargarAll()
-                          console.clear 
-                          this.create('Estudiantes Desasignados')
-                          $('#Loading').css('display','none')
-                        }).catch(error => {
-                          console.clear     
-                          this.createError(error) 
-                        })
-      
-    }
-    insert(formValue:any){
-      $('#Loading').css('display','block')
-      $('#Loading').addClass('in')
-      formValue = {
-        "tutor":this.selectedParent,
-        "students": this.droppedItemsId
-      }
+  onItemRemove(e: any) {
+    // Get the dropped data here
 
-      let formValueDel = {
-        "tutor":this.selectedParent,
-        "students": this.childsId
-      }
-          
-    if(this.selectedParent){      
+    this.childsId.push({ 'id': e.dragData.id });
+    this.childs.push(e.dragData);
+    this.selectedData.splice(this.selectedData.findIndex(dat => {
+      return dat.id === e.dragData.id;
+    }), 1);
+    this.droppedItemsId.splice(this.droppedItemsId.findIndex(dat => {
+      return dat.id === e.dragData.id;
+    }), 1);
+
+
+
+  }
+  limpiar() {
+    if (this.selectedData) {
+      this.selectedData.length = 0;
+      this.droppedItemsId.length = 0;
+      this.droppedItems.length = 0;
+      this.selectedParent = null;
+    }
+  }
+  cargarFree() {
+    $('#Loading').css('display', 'block');
+    $('#Loading').addClass('in');
+    this.ChildsService.getFreeStudents()
+      .then(response => {
+        this.childs = response;
+        this.childs.forEach((item, index) => {
+          this.childsId.push({ 'id': item.id });
+        });
+      }).catch(error => {
+        this.createError(error);
+      });
+  }
+  cargarAll() {
+    $('#Loading').css('display', 'block');
+    $('#Loading').addClass('in');
+    this.ParentsService.getBussy()
+      .then(response => {
+        this.Table = response;
+        $('#editModal .close').click();
+        $('#insertModal .close').click();
+        $('#Loading').css('display', 'none');
+      }).catch(error => {
+        this.createError(error);
+      });
+  }
+  cargarSingle(id: number) {
+    $('#Loading').css('display', 'block');
+    $('#Loading').addClass('in');
+    this.selectedParent = id;
+    if (this.droppedItemsId.length) {
+      this.droppedItemsId.length = 0;
+    }
+    this.cargarFree();
+    this.mainService.getMyChilds(id)
+      .then(response => {
+        this.selectedData = response;
+        this.selectedData.forEach((item, index) => {
+          this.droppedItemsId.push({ 'id': item.id });
+        });
+        $('#Loading').css('display', 'none');
+
+      }).catch(error => {
+        this.createError(error);
+      });
+  }
+  update(formValue: any) {
+    $('#Loading').css('display', 'block');
+    $('#Loading').addClass('in');
+    this.mainService.update(formValue)
+      .then(response => {
+        this.cargarAll();
+        this.create('Ciclo Actualizado exitosamente');
+        $('#Loading').css('display', 'none');
+      }).catch(error => {
+        this.createError(error);
+      });
+
+  }
+  delete2(id: string) {
+    this.mainService.delete(id)
+      .then(response => {
+        this.cargarAll();
+        this.create('Estudiantes Desasignados');
+      }).catch(error => {
+        this.createError(error);
+      });
+
+  }
+  delete(formValue) {
+    this.mainService.deleteAll(formValue)
+      .then(response => {
+        this.cargarAll();
+        this.create('Estudiantes Desasignados');
+        $('#Loading').css('display', 'none');
+      }).catch(error => {
+        this.createError(error);
+      });
+
+  }
+  insert(formValue: any) {
+    $('#Loading').css('display', 'block');
+    $('#Loading').addClass('in');
+    formValue = {
+      'tutor': this.selectedParent,
+      'students': this.droppedItemsId
+    };
+
+    const formValueDel = {
+      'tutor': this.selectedParent,
+      'students': this.childsId
+    };
+
+    if (this.selectedParent) {
       this.mainService.create(formValue)
-                        .then(response => {
-                          this.create('Estudiantes Asignados')
-                        }).catch(error => {
-                          console.clear     
-                          this.createError(error) 
-                        })
-      
-                      }else{
-                        this.createError("Debe seleccionar un Tutor") 
-                      }
-    this.delete(formValueDel)
-    }
-    
-  public options = {
-               position: ["bottom", "right"],
-               timeOut: 2000,
-               lastOnBottom: false,
-               animate: "fromLeft",  
-               showProgressBar: false,
-               pauseOnHover: true,
-               clickToClose: true,
-               maxLength: 200
-           };
-  
-    create(success) {
-                this._service.success('¡Éxito!',success)
+        .then(response => {
+          this.create('Estudiantes Asignados');
+        }).catch(error => {
+          this.createError(error);
+        });
 
+    } else {
+      this.createError('Debe seleccionar un Tutor');
     }
-    createError(error) {
-                this._service.error('¡Error!',error)
+    this.delete(formValueDel);
+  }
 
-    }
+  create(success) {
+    this._service.success('¡Éxito!', success);
+
+  }
+  createError(error) {
+    this._service.error('¡Error!', error);
+
+  }
 }
